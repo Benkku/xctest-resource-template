f68b852abaf006d7173e78efd7a34a639b7881d2
Pekka Rantala <pekka.rantala@siili.com>
2020-02-05 23:37:47 +0200

    Create ExampleSuiteResources.swift

---
+++ ExampleSuiteResources.swift
@@ -0,0 +1,9 @@
+import XCTest
+
+class ExampleSuiteResources: CommonResources {
+    let exampleSuiteConstant = "Example string"
+
+    func exampleSuiteFunction(param: String) {
+        
+    }
+}
