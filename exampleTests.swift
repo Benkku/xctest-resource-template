d67585125263d729b06ccab327bec08e6465537c
Pekka Rantala <pekka.rantala@siili.com>
2020-02-05 22:42:31 +0100

    Create exampleTests.swift

---
+++ exampleTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+class ExampleTests: ExampleSuiteResources {
+    let exampleTestConstant = "Aktia High Conviction B"
+   
+    override func setUp() {
+        super.setUp()
+        // Example test specific setup here
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        // Example test specific teardown here
+    }
+    
+    func testExample() {
+    // example test implementation here
+    }
+}
